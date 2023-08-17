31ee034c2b8e9e0826959e45aea5b138abb71e33
Levis Kim <122298089+Orekihotarou-k@users.noreply.github.com>
2023-08-17 15:57:17 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+// VARIABLES
+let faq = document.getElementsByClassName('content-container')
+
+for ( i = 0; i < faq.length; i++){
+    faq[i].addEventListener('click', function(){
+        this.classList.toggle('active')
+    })
+}
